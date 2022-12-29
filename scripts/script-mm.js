c41863548f8a56e326d06290200ea0ea0c797990
unknown <dima>
2022-12-29 10:27:39 +0300

    mobile menu in articleses

--- scripts/script-mm.js
+++ scripts/script-mm.js
@@ -2,10 +2,6 @@ let side = document.querySelector('#mobile-menu'),
 	btn = document.querySelector('#mobile-menu-btn'),
 	btnClose = document.querySelector('#mobile-menu-btn-close')
 
-console.log(side)
-console.log(btn)
-console.log(btnClose)
-
 btn.addEventListener('click', () => {
   side.classList.add('open-side')	
 })
