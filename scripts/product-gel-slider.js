c424250aa8ea417b189bffa9dcbfaf55f1bf2aee
unknown <dima>
2023-01-10 21:35:57 +0300

    add 2 sliders products with styles

--- scripts/product-gel-slider.js
+++ scripts/product-gel-slider.js
@@ -8,10 +8,10 @@ $('.owl-carousel').owlCarousel({
             items:1
         },
         600:{
-            items:1
+            items:3
         },
         1000:{
-            items:1
+            items:4
         }
     }
 })
